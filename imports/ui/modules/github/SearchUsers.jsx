8250517989c082f9751882405573f053eb919216
Robert Samuel <robert_samuel97@hotmail.com>
2021-02-06 04:21:49 -0600

    Github - Add the UsersList component Github - Create the GithubRequests file ClientRequests - Rename HttpRequestModule to ClientRequests

--- imports/ui/modules/github/SearchUsers.jsx
+++ imports/ui/modules/github/SearchUsers.jsx
@@ -4,6 +4,7 @@ import '/imports/ui/styles/User.css';
 import { useHistory } from 'react-router';
 import ArrayUtils from '/lib/ArrayUtils';
 import { StringUtils } from '/lib/StringUtils';
+import { GithubRequests } from '/lib/httpModules/GithubRequests';
 
 /*Material UI*/
 import Avatar from '@material-ui/core/Avatar';
@@ -12,6 +13,8 @@ import green from '@material-ui/core/colors/green';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+const http = new GithubRequests();
 
 const useStyles = makeStyles({
     container: {
@@ -52,12 +55,33 @@ const validateSearching = (user) => {
     return enabledSearch;
 };
 
+function UsersList(usersList) {
+    const history = useHistory();
+    const classes = useStyles();
+    const { users } = usersList;
+
+    return (
+        <div className='users-list-container'>
+            {users.map((user, index) => (
+                <div
+                    key={`${index}-${user.id}`}
+                    className='user-container'
+                >
+                    <Avatar className={classes.avatar} src={user.avatar_url} alt='avatar' />
+                    <label><strong>ID:</strong> {user.id}</label>
+                    <label><strong>User:</strong> {user.login}</label>
+                </div>
+            ))}
+        </div>
+    );
+}
+
 function SearchUsers() {
     const classes = useStyles();
     const [ user, setUser ] = useState('');
     const [ users, setUsers ] = useState([]);
-    // const [ hasError, setHasError ] = useState(false);
-    // const [ errorMessage, setErrorMessage ] = useState('');
+    const [ hasError, setHasError ] = useState(false);
+    const [ errorMessage, setErrorMessage ] = useState('');
     const [ isSearching, setIsSearching ] = useState(false);
     const isValidUser = validateSearching(user);
     const hasUsers = ArrayUtils.isNotEmpty(users);
@@ -67,7 +91,35 @@ function SearchUsers() {
     const subContainerStyle = hasUsers ? 'searching-finished-container' : 'searching-container';
 
     const fetchUsers = useCallback(async () => {
+        if(isValidUser) {
+            setHasError(false);
+            setIsSearching(true);
 
+            http.searchUsers(user).then(
+                result => {
+                    if(result) {
+                        if(result.statusCode === 200) {
+                            const data = result.data || {};
+                            const retrievedUsers = ArrayUtils.isNotEmpty(data.items)
+                                ? data.items.slice(0, 20)
+                                : [];
+
+                            setIsSearching(false);
+                            setUsers(retrievedUsers);
+                            console.log(retrievedUsers);
+                        } else if(result.statusCode !== 404) {
+                            setHasError(true);
+                            setErrorMessage(result.message);
+                        }
+                    }
+                },
+                reject => {
+                    setHasError(true);
+                    setErrorMessage(reject.message);
+                    setIsSearching(false);
+                }
+            );
+        }
     }, [user, isValidUser]);
 
     return(
@@ -120,6 +172,13 @@ function SearchUsers() {
                     </Button>
                 )}
             </div>
+            {hasUsers && (
+                <div className="searching-result-container">
+                    <div className="searching-result-subcontainer">
+                        <UsersList users={users} />
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
